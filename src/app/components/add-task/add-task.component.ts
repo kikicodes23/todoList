904ca32f62a927a64bad0bc7358bbe20690ec344
kikicodes23 <00117420@uca.edu.sv>
2025-12-04 04:14:10 -0600

    feat: update design for header and navigation bar

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { Task } from "../task/task";
 
 @Component({
   selector: 'app-add-task',
-  imports: [Task],
   templateUrl: './add-task.component.html',
 })
 export class AddTask {
-  
+
 }
--- src/app/pages/tasks/tasks.component.ts
+++ src/app/pages/tasks/tasks.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AddTask } from "../../components/add-task/add-task.component";
-import { Task } from "../../components/task/task";
 
 @Component({
   selector: 'app-tasks',
