900340fb09b4a3c5f21884b265f200b1710f1a21
kikicodes23 <00117420@uca.edu.sv>
2025-12-04 09:31:22 -0600

    feat: add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./components/shared/navbar/navbar.component";
 import { ScrollTopComponent } from "./components/shared/scroll-top/scroll-top.component";
+import { FooterComponent } from "./components/shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   //standalone: true,
-  imports: [RouterOutlet, Navbar, ScrollTopComponent],
+  imports: [RouterOutlet, Navbar, ScrollTopComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
